22f990469591aae2812dc2b070d1efe81044e866
Kevin Henrique da Silva <kevin.silva@softtek.com>
2023-02-08 16:57:37 -0300

    fix: novos ajustes de camadas e teste unitario

--- src/app/informacao-pessoal/informacao-pessoal.component.spec.ts
+++ src/app/informacao-pessoal/informacao-pessoal.component.spec.ts
@@ -1,8 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { InformacaoPessoalComponent } from './informacao-pessoal.component';
 
 describe('InformacaoPessoalComponent', () => {
+
+  let component: InformacaoPessoalComponent = new InformacaoPessoalComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -19,5 +23,26 @@ describe('InformacaoPessoalComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('Informação Pessoal foi criado com dados', () => {
+
+    component.dadoPessoal = {
+      nome: 'João Lucas', duplicarContaAplicacao: false,
+      duplicarContaCorrente: false, contaAplicacao: '5512932-8',
+      contaCorrente: '718069-4', situacaoCpf: true, cpf: '77498258005'
+    };
+
+    expect(component.dadoPessoal).toBeTruthy();
+  })
+
+  it('Deve ter um título com a situação cadastral', () => {
+    const fixture = TestBed.createComponent(InformacaoPessoalComponent);
+
+    fixture.detectChanges();
+
+    let cadastral = fixture.debugElement.query(By.css('.card-title')).nativeElement;
+
+    expect(cadastral.innerText).toContain('Situação cadastral do CPF');
+  })
 
 });
--- src/app/valida-cpf/valida-cpf.component.spec.ts
+++ src/app/valida-cpf/valida-cpf.component.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatStepperModule } from '@angular/material/stepper';
+import { BrowserModule, By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { DadosPessoaisService } from '../servico/dados-pessoais.service';
@@ -13,7 +15,10 @@ describe('ValidaCpfComponent', () => {
         RouterTestingModule,
         HttpClientModule,
         MatStepperModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        FormsModule,
+        BrowserModule,
+        ReactiveFormsModule
       ],
       declarations: [
         ValidaCpfComponent
@@ -28,4 +33,35 @@ describe('ValidaCpfComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  it('Verifica se existe uma class para CPF', () => {
+    const fixture = TestBed.createComponent(ValidaCpfComponent);
+
+    fixture.detectChanges();
+
+    let cpf = fixture.debugElement.query(By.css('.campoCpf')).nativeElement;
+
+    expect(cpf.innerText).toContain('CPF');
+  })
+
+  it('Valida se há algum dados na Classe DadosPessoais', () => {
+    const fixture = TestBed.createComponent(ValidaCpfComponent);
+
+    fixture.detectChanges();
+    let component = fixture.componentInstance;
+    expect(component.dados).toBeUndefined();
+  })
+
+
+  it('Verifica se foi chamado o metodo consultaDadosPessoais', () => {
+    const fixture = TestBed.createComponent(ValidaCpfComponent);
+
+    fixture.detectChanges();
+    let component = fixture.componentInstance;
+
+    component.formulario.get('cpf')?.setValue('42456809033');
+
+    component.consultar();
+
+    expect(component.ativaCarregamento).toEqual(false);
+  })
 });
